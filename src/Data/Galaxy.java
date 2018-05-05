43dd2f9165ad12c24cc07ce155db71929d31391e
pinggon <fixp101@gmail.com>
2018-05-05 13:53:57 +0200

    Added findSystem and findShips to Galaxy

--- src/Data/Galaxy.java
+++ src/Data/Galaxy.java
@@ -8,6 +8,21 @@ import java.util.List;
 
 public class Galaxy {
     List<GameSystem> gameSystemList = new ArrayList<>();
+    private int amountOfSystems = 0;
+    private int amountOfShips = 0;
+    private int amountOfPlanets = 0;
+    
+    public void findSystemsInGalaxy() {
+        amountOfSystems += gameSystemList.size();
+        System.out.println("There is " + amountOfSystems + " systems in the galaxy");
+    }
+    public void findShipsInGalaxy(){
+        for (GameSystem gameSystem: gameSystemList) {
+            amountOfShips += gameSystem.findShips();
+        }
+        System.out.println("There is " + amountOfShips + " in the galaxy");
+    }
+    public void findPlanetsInGalaxy(){
 
-
+    }
 }
